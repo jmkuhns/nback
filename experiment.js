d58d9fef0e7c185627de6c460000a139fd1d6966
jmkuhns <jmkuhns@uncg.edu>
2020-09-01 17:32:58 -0400

    diff_alg.1

--- experiment.js
+++ experiment.js
@@ -90,7 +90,7 @@ for (var i = 2; i <= stims_prac.length; i++){
 /*define static blocks */
 var welcome = {
   type: "html-keyboard-response",
-  stimulus: '<p>Welcome to the experiment. Press any key to begin.' + stims_prac '</p>'
+  stimulus: '<p>Welcome to the experiment. Press any key to begin.</p>'
 };
 
 /// This ensures that the subject does not read through the instructions too quickly.  If they do it too quickly, then we will go over the loop again.
@@ -109,7 +109,7 @@ var instructions_block = {
 var start_practice_block = {
 	type: "html-keyboard-response",
 	stimulus:
-	'<p>Starting practice.<br>During practice, you should press the left arrow key when the current number matches the number that appeared 2 trials before. Otherwise press the right arrow key.</p><p>You will receive feedback about whether you were correct or not during practice. There will be no feedback during the main experiment. Press any key to begin.<br>'	+	success_prac_init+ '<br>'+ stims_prac	+	'</p>',
+	'<p>Starting practice.<br>During practice, you should press the left arrow key when the current number matches the number that appeared 2 trials before. Otherwise press the right arrow key.</p><p>You will receive feedback about whether you were correct or not during practice. There will be no feedback during the main experiment. Press any key to begin.<br>' + stims_prac	+	'</p>',
 	data: {
 		trial_id: "instruction"
 	},
