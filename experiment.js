64d2beaa306af20417fd172143f1a51bda1bbdcd
jmkuhns <jmkuhns@uncg.edu>
2020-09-03 17:57:06 -0400

    remove 0 from stim set

--- experiment.js
+++ experiment.js
@@ -36,7 +36,7 @@ var randomDraw = function(lst) {
 
 // task specific variables
 var current_trial = 0;
-var numbers = [0,1,2,3,4,5,6,7,8,9];
+var numbers = [1,2,3,4,5,6,7,8,9];
 var success = [0,1];
 var num_blocks = 6;
 var num_trials = 20;
