6c33ac45605c91673b57444c5fbf47edacf82621
jmkuhns <jmkuhns@uncg.edu>
2020-09-08 13:56:26 -0400

    instructions tweaks 1.5

--- experiment.js
+++ experiment.js
@@ -120,7 +120,7 @@ var instructions_block = {
 var start_practice_block = {
 	type: "html-keyboard-response",
 	stimulus:
-	'<p>Starting practice.<br><br>During practice, you should press the left arrow key when the current number matches the number that appeared 2 trials before. Otherwise press the right arrow key. This means that for the first two trials, you should press the right arrow key, because there are no numbers 2 trials before.</p><br><br><p>You will receive feedback about whether you were correct or not during practice. There will be no feedback during the main experiment. Press any key to begin.</p>',
+	'<p>Starting practice.<br><br>During practice, you should press the left arrow key when the current number matches the number that appeared 2 trials before. Otherwise press the right arrow key. This means that for the first two trials, you should press the right arrow key, because there are no numbers 2 trials before.</p><br><p>You will receive feedback about whether you were correct or not during practice. There will be no feedback during the main experiment.<br><br> Press any key to begin.</p>',
 	data: {
 		exp_stage: "instruction"
 	},
