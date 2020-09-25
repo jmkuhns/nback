a418aea020dfc94410e83d3d558b192f0c474497
jmkuhns <jmkuhns@uncg.edu>
2020-09-25 13:14:13 -0400

    shortening length for debug

--- nback.js
+++ nback.js
@@ -217,7 +217,7 @@ var test_brief = {
 	stimulus: '<p>You have now completed the practice trials. The experiment will consist of 6 blocks of 20 trials each.</p><br><p style:"font-size:25px";>Remember, there will not be feedback from this point on.</p><br><p>Press any key to begin block 1.</p>'
 };
 timeline.push(test_brief);
-var num_blocks = 7;
+var num_blocks = 2;
 var b = 1;
 while (b < num_blocks){
 		var test_inter = {
